88be59cd5958051a7093f95447b7492b652f6912
liza <lizavetanavitskaya98@gmail.com>
2022-02-05 21:48:49 +0300

    HW2

--- src/com/liza/idea/HW3_task3_accumulator.java
+++ src/com/liza/idea/HW3_task3_accumulator.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 
 public class HW3_task3_accumulator {
     Scanner in = new Scanner(System.in);
-    int ArrayNUmbers[] = new int[5];
+    int ArrayNumbers[] = new int[5];
 
 }
